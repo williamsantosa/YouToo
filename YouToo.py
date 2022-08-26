b224286b05049b4881237eb226ff7c95f3fad093
Will <williamwsantosa@gmail.com>
2022-08-26 01:00:21 -0700

    grid

--- YouToo.py
+++ YouToo.py
@@ -2,27 +2,30 @@ import pytube as pt, sys
 import logging
 from pytube import YouTube
 from PyQt6.QtCore import Qt, QSize
-from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton
+from PyQt6.QtWidgets import *
 
 class YouToo(QMainWindow):
   def __init__(self):
     super().__init__()
 
+    self.yt_link = ""
+
     self.setWindowTitle("YouToo")
-    self.setFixedSize(QSize(400, 300))
+    self.setFixedSize(400, 300)
 
-    button = QPushButton(text = "Download!")
-    button.clicked.connect(self.on_button_download)
+    self.grid = QGridLayout()
 
-    self.setCentralWidget(button)
+    button_download = QPushButton(text = "Download!")
+    button_download.clicked.connect(self.on_button_download)
     
+    self.setCentralWidget(button)
+
   def on_button_download(self):
     logging.info("on_button_download pressed")
 
 if __name__ == "__main__":
   logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
-  yt = YouTube('https://www.youtube.com/watch?v=8V4bPy4w3Hc&ab_channel=Avatar%3ATheLastAirbender')
-  
+
   app = QApplication([])
   
   window = YouToo()
