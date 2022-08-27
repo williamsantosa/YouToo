d664a864a6f6e9be3c556c8b6f1eb8a2d20a6cb0
Will <williamwsantosa@gmail.com>
2022-08-27 14:30:46 -0700

    Trying out PyQt6

--- YouToo.py
+++ YouToo.py
@@ -8,25 +8,46 @@ class YouToo(QMainWindow):
   def __init__(self):
     super().__init__()
 
+    # YouTube Link
     self.yt_link = ""
 
+    # Create Window
     self.setWindowTitle("YouToo")
-    self.setFixedSize(400, 300)
+    #self.setFixedSize(400, 300)
 
+    # Set QMainWindow layout to grid
     self.grid = QGridLayout()
+    self.widget = QWidget()
+    self.widget.setLayout(self.grid)
+    self.setCentralWidget(self.widget)
 
-    button_download = QPushButton(text = "Download!")
-    button_download.clicked.connect(self.on_button_download)
+    # Create widgets
+    self.button_download = QPushButton(text = "Download!")
+    self.button_file_name = QPushButton(text = 'File Name')
+    self.le = QLabel(text = '')
+
+    # Connect events
+    self.button_download.clicked.connect(self.on_button_download)
+    self.button_file_name.clicked.connect(self.on_button_file_name)
     
-    self.setCentralWidget(button)
+    # Add widgets to the screen
+    self.grid.addWidget(self.button_file_name, 0, 0, 1, 1)
+    self.grid.addWidget(self.le, 1, 0, 1, 1)
+    self.grid.addWidget(self.button_download, 2, 0, 1, 1)
 
   def on_button_download(self):
     logging.info("on_button_download pressed")
 
+  def on_button_file_name(self):
+    text, ok = QInputDialog.getText(self, 'Input Dialog', 'Enter your name:')
+
+    if ok:
+      self.le.setText(str(text))
+
 if __name__ == "__main__":
   logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
 
-  app = QApplication([])
+  app = QApplication(sys.argv)
   
   window = YouToo()
   window.show()
